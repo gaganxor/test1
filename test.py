777d1acc844ee3a66ad84fb97309ca20b8afc822
gaganxor <50016474+gaganxor@users.noreply.github.com>
2019-04-26 16:03:16 +0530

    test py file
    testing purpose

---
+++ test.py
@@ -0,0 +1,30 @@
+#!C:\Users\XOR SOLUTIONS\AppData\Local\Programs\Python\Python37-32\python.exe
+
+print("Content-Type: text/html\n")
+print ("Hello Python Web Browser!! This is cool!!" + "<br>")
+
+
+
+def myFunc(e):
+  return e['year']
+
+cars = [
+  {'car': 'Ford', 'year': 2005},
+  {'car': 'Mitsubishi', 'year': 2000},
+  {'car': 'BMW', 'year': 2019},
+  {'car': 'VW', 'year': 2011}
+]
+
+cars.sort(key=myFunc)
+
+print(cars)
+
+fruits = ["apple", "banana", "cherry"]
+
+points = [1,4,5,9]
+
+fruits.extend(points)
+
+print(fruits)
+
+
